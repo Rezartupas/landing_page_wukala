91d3b5006275390ca103d5e1f49c3dce549c0129
Reza Saputra <rezagondol@gmail.com>
2025-01-27 22:48:10 +0700

    Refactor scrolling logic and enhance cursor indicator functionality

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ let currentSection = 0;
 const sections = document.querySelectorAll('.section');
 const cursorIndicator = document.getElementById('cursorIndicator');
 
+function updateCursorIndicator() {
+    if (currentSection === 2) {
+        cursorIndicator.classList.add('cursor-indicator-logo');
+        cursorIndicator.classList.remove('cursor-indicator');
+    } else {
+        cursorIndicator.classList.add('cursor-indicator');
+        cursorIndicator.classList.remove('cursor-indicator-logo');
+    }
+}
+
 window.addEventListener('wheel', function(event) {
     if (event.deltaY > 0) {
         // Scrolling down
@@ -23,13 +33,55 @@ window.addEventListener('wheel', function(event) {
             sections[currentSection].scrollIntoView({ behavior: 'smooth' });
         }
     }
-
-    // Change cursor indicator when entering section 3
-    if (currentSection === 2) {
-        cursorIndicator.classList.add('cursor-indicator-logo');
-        cursorIndicator.classList.remove('cursor-indicator');
-    } else {
-        cursorIndicator.classList.add('cursor-indicator');
-        cursorIndicator.classList.remove('cursor-indicator-logo');
-    }
+    updateCursorIndicator();
 });
+
+window.addEventListener('touchstart', handleTouchStart, false);
+window.addEventListener('touchmove', handleTouchMove, false);
+
+let xDown = null;
+let yDown = null;
+
+function handleTouchStart(evt) {
+    const firstTouch = evt.touches[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    let xUp = evt.touches[0].clientX;
+    let yUp = evt.touches[0].clientY;
+
+    let xDiff = xDown - xUp;
+    let yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        // Most significant
+        if (xDiff > 0) {
+            /* left swipe */
+        } else {
+            /* right swipe */
+        }
+    } else {
+        if (yDiff > 0) {
+            // Scrolling down
+            if (currentSection < sections.length - 1) {
+                currentSection++;
+                sections[currentSection].scrollIntoView({ behavior: 'smooth' });
+            }
+        } else {
+            // Scrolling up
+            if (currentSection > 0) {
+                currentSection--;
+                sections[currentSection].scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    }
+    updateCursorIndicator();
+    xDown = null;
+    yDown = null;
+}
